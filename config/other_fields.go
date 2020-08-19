8f613c5d64f6f41f12f30adead57cb4843400b13
Yiting Qiang <qiangyt@wxcount.com>
2020-08-19 12:17:06 +0800

    move SeparatorField out from other_fields.go

--- config/other_fields.go
+++ config/other_fields.go
@@ -1,57 +1,5 @@
 package config
 
-import (
-	"github.com/gookit/goutil/strutil"
-	"github.com/qiangyt/jog/util"
-)
-
-// SeparatorFieldT ...
-type SeparatorFieldT struct {
-	ElementT
-
-	Label string
-}
-
-// SeparatorField ...
-type SeparatorField = *SeparatorFieldT
-
-// UnmarshalYAML ...
-func (i SeparatorField) UnmarshalYAML(unmarshal func(interface{}) error) error {
-	return util.UnmarshalYAML(i, unmarshal)
-}
-
-// MarshalYAML ...
-func (i SeparatorField) MarshalYAML() (interface{}, error) {
-	return util.MarshalYAML(i)
-}
-
-// Reset ...
-func (i SeparatorField) Reset() {
-	i.ElementT.Reset()
-
-	i.Label = "="
-}
-
-// FromMap ...
-func (i SeparatorField) FromMap(m map[string]interface{}) error {
-	if err := i.ElementT.FromMap(m); err != nil {
-		return err
-	}
-
-	labelV := util.ExtractFromMap(m, "label")
-	if labelV != nil {
-		i.Label = strutil.MustString(labelV)
-	}
-	return nil
-}
-
-// ToMap ...
-func (i SeparatorField) ToMap() map[string]interface{} {
-	r := i.ElementT.ToMap()
-	r["label"] = i.Label
-	return r
-}
-
 // OtherFieldsT ...
 type OtherFieldsT struct {
 	Name      Element
