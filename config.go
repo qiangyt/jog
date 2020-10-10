qiangyt/jog

commit cea3edbb5f6c19079e21688d657a85a5587d4394

Fix: failed to load default configuration file due to a stupid error that takes path as yaml

config.go
@@ -154,11 +154,11 @@ func ConfigWithDefaultYamlFile() Config {
 
 	if len(path) == 0 {
 		log.Println("config file not found, take default config")
-		return ConfigWithYaml(config.DefaultYAML)
+		return ConfigWithYamlFile(config.DefaultYAML)
 	}
 
 	log.Printf("config file: %s\n", path)
-	return ConfigWithYaml(path)
+	return ConfigWithYamlFile(path)
 }
 
 // ConfigWithYamlFile ...

commit 7af2ac96fda9027eda207f3df8cf6f83da901647

fix: regression by https://github.com/qiangyt/jog/commit/cea3edbb5f6c19079e21688d657a85a5587d4394

config.go
@@ -154,7 +154,7 @@ func ConfigWithDefaultYamlFile() Config {
 
 	if len(path) == 0 {
 		log.Println("config file not found, take default config")
-		return ConfigWithYamlFile(config.DefaultYAML)
+		return ConfigWithYaml(config.DefaultYAML)
 	}
 
 	log.Printf("config file: %s\n", path)