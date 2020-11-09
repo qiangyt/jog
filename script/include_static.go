147ac8307e92c2834a4511e29656dac4cbfe3a23
Yiting Qiang <qiangyt@wxcount.com>
2020-11-09 16:15:59 +0000

    update static file include script

--- script/include_static.go
+++ script/include_static.go
@@ -5,56 +5,81 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+	"path"
 	"strings"
 )
 
-const (
-	// BaseDir ...
-	BaseDir = "./static_files"
-)
+func includeDir(staticGoParentDir string, staticFileParentDir string, dirName string) {
+	staticGoDir := path.Join(staticGoParentDir, dirName)
+	os.MkdirAll(staticGoDir, os.ModePerm)
 
-// Reads all static files in the current folder
-// and encodes them as strings literals in static/static.go
-func main() {
-	os.MkdirAll("static", os.ModePerm)
-
-	fs, _ := ioutil.ReadDir(BaseDir)
+	staticFilesDir := path.Join(staticFileParentDir, dirName)
+	fs, _ := ioutil.ReadDir(staticFilesDir)
 
 	for _, f := range fs {
 		fName := f.Name()
 
-		fmt.Println("Including static file: " + fName)
-
-		var fTitle, fExt string
-
-		indexOfLastDot := strings.LastIndex(fName, ".")
-		if indexOfLastDot >= 0 {
-			fTitle = fName[:indexOfLastDot]
-			fExt = fName[indexOfLastDot+1:]
+		if f.IsDir() {
+			includeDir(staticGoDir, staticFilesDir, fName)
 		} else {
-			fTitle = fName
-			fExt = ""
+			includeFile(staticGoDir, staticFilesDir, fName)
 		}
+	}
+}
 
-		indexOfLastSlash := strings.LastIndex(fTitle, "/")
-		if indexOfLastSlash >= 0 {
-			fTitle = fTitle[indexOfLastSlash+1:]
+func includeFile(staticGoParentDir string, staticFileParentDir string, fName string) {
+	packageName := staticGoParentDir[strings.LastIndex(staticGoParentDir, "/")+1:]
+
+	fPath := path.Join(staticFileParentDir, fName)
+	fmt.Println("Including static file: " + fPath)
+
+	var fTitle, fExt string
+
+	indexOfLastDot := strings.LastIndex(fName, ".")
+	if indexOfLastDot >= 0 {
+		fTitle = fName[:indexOfLastDot]
+		fExt = fName[indexOfLastDot+1:]
+	} else {
+		fTitle = fName
+		fExt = ""
+	}
+
+	indexOfLastSlash := strings.LastIndex(fTitle, "/")
+	if indexOfLastSlash >= 0 {
+		fTitle = fTitle[indexOfLastSlash+1:]
+	}
+
+	// TODO: this code does not work for text files that contain: %  `
+
+	out, _ := os.Create(path.Join(staticGoParentDir, fName+".go"))
+	defer out.Close()
+	out.WriteString("package " + packageName + " \n\nconst (\n")
+
+	varName := fTitle
+	if len(fExt) > 0 {
+		varName = varName + "_" + fExt
+	}
+	out.WriteString("  " + varName + " = `")
+
+	f, _ := os.Open(fPath)
+	defer f.Close()
+	io.Copy(out, f)
+
+	out.WriteString("`\n")
+	out.WriteString(")\n")
+}
+
+// Reads all static files in the current folder
+// and encodes them as strings literals in static/static.go
+func main() {
+	fs, _ := ioutil.ReadDir("./static_files")
+
+	for _, f := range fs {
+		fName := f.Name()
+		if f.IsDir() {
+			includeDir("./static", "./static_files", fName)
+		} else {
+			includeFile("./static", "./static_files", fName)
 		}
-
-		// TODO: this code does not work for text files that contain: %  `
-
-		out, _ := os.Create("static/" + fName + ".go")
-		out.WriteString("package static \n\nconst (\n")
-
-		varName := fTitle
-		if len(fExt) > 0 {
-			varName = varName + "_" + fExt
-		}
-		out.WriteString("  " + varName + " = `")
-
-		f, _ := os.Open(BaseDir + "/" + fName)
-		io.Copy(out, f)
-		out.WriteString("`\n")
-		out.WriteString(")\n")
 	}
 }
