7cf1ec715cf7f1bd668ebe52a38c7c7204234156
Yiting Qiang <qiangyt@wxcount.com>
2020-11-14 03:00:05 +0800

    fix travis ci

---
+++ static/grok_vjeantet/README.md.go
@@ -0,0 +1,8 @@
+package grok_vjeantet 
+
+const (
+  // README_md ...
+  README_md string = `The default GROK patterns in ~/.jog/grok_vjeantet are copy of https://github.com/vjeantet/grok/tree/master/patterns v1.0.0
+
+https://github.com/vjeantet/grok LICENSE file: ~/.jog/grok_vjeantet.LICENSE`
+)
