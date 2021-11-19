1265dbe6068047af30d63553e944051085152792
Yiting Qiang <qiangyt@wxcount.com>
2021-11-20 01:05:55 +0800

    util/tail_queue_test.go: switch to testify

--- util/tail_queue_test.go
+++ util/tail_queue_test.go
@@ -1,185 +1,107 @@
 package util
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func Test_initialization(t *testing.T) {
+	assert := require.New(t)
+
 	r := NewTailQueue(3)
 
-	if r.Count() != 0 || !r.IsEmpty() {
-		t.Error("q should be empty when just initialized")
-	}
-
-	if r.IsFull() {
-		t.Error("q should be not full when just initialized")
-	}
-
-	if r.Head() != nil {
-		t.Error("head element should be nil")
-	}
-	if r.Tail() != nil {
-		t.Error("tail element should be nil")
-	}
+	assert.False(r.Count() != 0 || !r.IsEmpty(), "q should be empty when just initialized")
+	assert.False(r.IsFull(), "q should be not full when just initialized")
+	assert.Nil(r.Head(), "head element should be nil")
+	assert.Nil(r.Tail(), "tail element should be nil")
 }
 
 func Test_Add_aLittle(t *testing.T) {
+	assert := require.New(t)
+
 	r := NewTailQueue(3)
 
 	r.Add("1")
 	r.Add("2")
-	if r.Count() != 2 || r.IsEmpty() {
-		t.Error("element amount should be 2")
-	}
-	if r.IsFull() {
-		t.Error("q(3) with 2 elements amount should not be full")
-	}
-	if r.Head() != "1" {
-		t.Error("head element should be '1'")
-	}
-	if r.Tail() != "2" {
-		t.Error("tail element should be '2'")
-	}
+	assert.False(r.Count() != 2 || r.IsEmpty(), "element amount should be 2")
+	assert.False(r.IsFull(), "q(3) with 2 elements amount should not be full")
+	assert.Equal("1", r.Head(), "head element should be '1'")
+	assert.Equal("2", r.Tail(), "tail element should be '2'")
 
 	e1 := r.Kick()
-	if e1 != "1" {
-		t.Error("the kicked element 1 should be '1'")
-	}
-	if r.Count() != 1 || r.IsEmpty() {
-		t.Error("after kicked one, element amount should be 1")
-	}
-	if r.IsFull() {
-		t.Error("after kicked one, q(3) with 2 elements amount should not be full")
-	}
-	if r.Head() != "2" {
-		t.Error("after kicked one, head element should be '2'")
-	}
-	if r.Tail() != r.Head() {
-		t.Error("after kicked one, tail element and head element should be same")
-	}
+	assert.Equal("1", e1, "the kicked element 1 should be '1'")
+	assert.False(r.Count() != 1 || r.IsEmpty(), "after kicked one, element amount should be 1")
+	assert.False(r.IsFull(), "after kicked one, q(3) with 2 elements amount should not be full")
+	assert.Equal("2", r.Head(), "after kicked one, head element should be '2'")
+	assert.False(r.Tail() != r.Head(), "after kicked one, tail element and head element should be same")
 
 	e2 := r.Kick()
-	if e2 != "2" {
-		t.Error("the kicked element 2 should be '2'")
-	}
-	if r.Count() != 0 || !r.IsEmpty() {
-		t.Error("after kicked two, element amount should be 0")
-	}
-	if r.IsFull() {
-		t.Error("after kicked two, q(3) with 2 elements amount should not be full")
-	}
-	if r.Head() != nil {
-		t.Error("after kicked two, head element should be nil")
-	}
-	if r.Tail() != nil {
-		t.Error("after kicked two, tail element should be nil")
-	}
-
+	assert.Equal("2", e2, "the kicked element 2 should be '2'")
+	assert.False(r.Count() != 0 || !r.IsEmpty(), "after kicked two, element amount should be 0")
+	assert.False(r.IsFull(), "after kicked two, q(3) with 2 elements amount should not be full")
+	assert.Nil(r.Head(), "after kicked two, head element should be nil")
+	assert.Nil(r.Tail(), "after kicked two, tail element should be nil")
 }
 
 func Test_Add_Full_then_Kick(t *testing.T) {
+	assert := require.New(t)
+
 	r := NewTailQueue(3)
 
 	r.Add("1")
 	r.Add("2")
 	r.Add("3")
 
-	if r.Count() != 3 || r.IsEmpty() {
-		t.Error("after add 3 element, should not be empty")
-	}
-	if !r.IsFull() {
-		t.Error("after add 3 element, should be full")
-	}
-	if r.Head() != "1" {
-		t.Error("head element should be '1'")
-	}
-	if r.Tail() != "3" {
-		t.Error("tail element should be '3'")
-	}
+	assert.False(r.Count() != 3 || r.IsEmpty(), "after add 3 element, should not be empty")
+	assert.True(r.IsFull(), "after add 3 element, should be full")
+	assert.Equal("1", r.Head(), "head element should be '1'")
+	assert.Equal("3", r.Tail(), "tail element should be '3'")
 
 	r.Add("4")
-	if r.Count() != 3 || r.IsEmpty() {
-		t.Error("after add 4 element, should not be empty")
-	}
-	if !r.IsFull() {
-		t.Error("after add 4 element, should be full")
-	}
-	if r.Head() != "2" {
-		t.Error("head element should be '2'")
-	}
-	if r.Tail() != "4" {
-		t.Error("tail element should be '4'")
-	}
+	assert.False(r.Count() != 3 || r.IsEmpty(), "after add 4 element, should not be empty")
+	assert.True(r.IsFull(), "after add 4 element, should be full")
+	assert.Equal("2", r.Head(), "head element should be '2'")
+	assert.Equal("4", r.Tail(), "tail element should be '4'")
 
 	r.Add("5")
 	r.Add("6")
 	r.Add("7")
 
-	if r.Count() != 3 || r.IsEmpty() {
-		t.Error("after add 7 element, should not be empty")
-	}
-	if !r.IsFull() {
-		t.Error("after add 7 element, should be full")
-	}
-	if r.Head() != "5" {
-		t.Error("head element should be '5'")
-	}
-	if r.Tail() != "7" {
-		t.Error("tail element should be '7'")
-	}
+	assert.False(r.Count() != 3 || r.IsEmpty(), "after add 7 element, should not be empty")
+	assert.True(r.IsFull(), "after add 7 element, should be full")
+	assert.Equal("5", r.Head(), "head element should be '5'")
+	assert.Equal("7", r.Tail(), "tail element should be '7'")
 
 	r.Add("8")
 	r.Add("9")
-	if r.Count() != 3 {
-		t.Error("element count should be 3")
-	}
+	assert.Equal(3, r.Count(), "element count should be 3")
 
-	if r.Kick() != "7" {
-		t.Error("should be '7'")
-	}
-	if r.Count() != 2 {
-		t.Error("element count should be 2")
-	}
-	if r.Kick() != "8" {
-		t.Error("should be '8'")
-	}
-	if r.Kick() != "9" {
-		t.Error("should be '9'")
-	}
+	assert.Equal("7", r.Kick(), "should be '7'")
+	assert.Equal(2, r.Count(), "element count should be 2")
+	assert.Equal("8", r.Kick(), "should be '8'")
+	assert.Equal("9", r.Kick(), "should be '9'")
 
-	if r.Count() != 0 {
-		t.Error("element count should be 0")
-	}
-	if !r.IsEmpty() {
-		t.Error("q should be empty")
-	}
+	assert.Equal(0, r.Count(), "element count should be 0")
+	assert.True(r.IsEmpty(), "q should be empty")
 
-	if r.Kick() != nil || r.Head() != nil || r.Tail() != nil {
-		t.Error("should no element")
-	}
+	assert.False(r.Kick() != nil || r.Head() != nil || r.Tail() != nil, "should no element")
 }
 
 func Test_Clear(t *testing.T) {
+	assert := require.New(t)
+
 	r := NewTailQueue(3)
 
 	r.Add("1")
 	r.Add("2")
 
-	if r.Count() != 2 || r.IsEmpty() {
-		t.Error("element amount should be 2")
-	}
+	assert.False(r.Count() != 2 || r.IsEmpty(), "element amount should be 2")
 
 	r.Clear()
 
-	if r.Count() != 0 || !r.IsEmpty() {
-		t.Error("after clear, should be empty")
-	}
-	if r.IsFull() {
-		t.Error("after clear, should not be full")
-	}
-	if r.Head() != nil {
-		t.Error("after clear, head element should be nil")
-	}
-	if r.Tail() != nil {
-		t.Error("after clear, tail element should be nil")
-	}
-
+	assert.False(r.Count() != 0 || !r.IsEmpty(), "after clear, should be empty")
+	assert.False(r.IsFull(), "after clear, should not be full")
+	assert.Nil(r.Head(), "after clear, head element should be nil")
+	assert.Nil(r.Tail(), "after clear, tail element should be nil")
 }
