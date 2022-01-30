aeeea5f9be6c210d7336d442a9c7bdd02cd88127
Yiting Qiang <qiangyt@wxcount.com>
2022-01-31 01:21:49 +0800

    Split help info to global + server + convert information

--- common/global_options.go
+++ common/global_options.go
@@ -71,19 +71,50 @@ func (me GlobalOptions) PrintErrorHint(format string, a ...interface{}) {
 
 // PrintHelp ...
 func (me GlobalOptions) PrintHelp() {
-	defaultGrokLibraryDirs := strings.Join(config.DefaultGrokLibraryDirs(false), ", ")
-
-	color.New(color.Blue, color.OpBold).Println("\nConvert and view structured (JSON) log")
+	color.New(color.Blue, color.OpBold).Println("\nJog: convert/view/share log")
 	me.PrintVersion()
 	fmt.Println()
 
-	color.New(color.FgBlue, color.OpBold).Println("Usage:")
+	color.New(color.FgBlue, color.OpBold).Println("Global options:")
+	fmt.Printf("  -d,  --debug                Print more error detail\n")
+	fmt.Printf("  -h,  --help                 Display this information\n")
+	color.New(color.FgGreen, color.OpBold).Printf("  server -h,  server --help   Display server mode help information\n")
+	fmt.Printf("  -V,  --version              Display app version information\n")
+	fmt.Println()
+
+	if me.RunMode == RunMode_Server {
+		me.PrintServerHelp()
+	} else {
+		me.PrintConvertHelp()
+	}
+}
+
+func (me GlobalOptions) PrintServerHelp() {
+	fmt.Println("server help: TODO")
+	fmt.Println()
+
+	color.New(color.FgBlue, color.OpBold).Println("Server mode usage:")
+	color.FgBlue.Println("  jog  server [option...]")
+	fmt.Println()
+
+	color.New(color.FgBlue, color.OpBold).Println("Servr mode options:")
+	fmt.Printf("  -c,  --config <server config file path>                     Specify server config YAML file path. The default is .jog.server.yaml \n")
+	fmt.Printf("  -cs, --config-set <config item path>=<config item value>    Set value to specified config item \n")
+	fmt.Printf("  -cg, --config-get <config item path>                        Get value to specified config item \n")
+	fmt.Printf("  -t,  --template                                             Print a server config YAML file template\n")
+	fmt.Println()
+}
+
+func (me GlobalOptions) PrintConvertHelp() {
+	defaultGrokLibraryDirs := strings.Join(config.DefaultGrokLibraryDirs(false), ", ")
+
+	color.New(color.FgBlue, color.OpBold).Println("Convert/view usage:")
 	color.FgBlue.Println("  jog  [option...]  <your JSON log file path>")
 	color.FgBlue.Println("    or")
 	color.FgBlue.Println("  cat  <your JSON file path>  |  jog  [option...]")
 	fmt.Println()
 
-	color.New(color.FgBlue, color.OpBold).Println("Examples:")
+	color.New(color.FgBlue, color.OpBold).Println("Convert/view examples:")
 	fmt.Println("   1) follow with last 10 lines:         jog -f app-20200701-1.log")
 	fmt.Println("   2) follow with specified lines:       jog -n 100 -f app-20200701-1.log")
 	fmt.Println("   3) with specified config file:        jog -c another.jog.yml app-20200701-1.log")
@@ -97,21 +128,18 @@ func (me GlobalOptions) PrintHelp() {
 	fmt.Println("   11) view apache log, non-JSON log     jog -g COMMONAPACHELOG example_logs/grok_apache.log")
 	fmt.Println()
 
-	color.New(color.FgBlue, color.OpBold).Println("Options:")
+	color.New(color.FgBlue, color.OpBold).Println("Convert/view options:")
 	fmt.Printf("  -a,  --after <timestamp>                                    'after' time filter. Auto-detect the timestamp format; can be natural datetime \n")
 	fmt.Printf("  -b,  --before <timestamp>                                   'before' time filter. Auto-detect the timestamp format; can be natural datetime \n")
-	fmt.Printf("  -c,  --config <config file path>                            Specify config YAML file path. The default is .jog.yaml or $HOME/.jog.yaml \n")
+	fmt.Printf("  -c,  --config <convertion config file path>                 Specify convertion config YAML file path. The default is .jog.yaml or $HOME/.jog.yaml \n")
 	fmt.Printf("  -cs, --config-set <config item path>=<config item value>    Set value to specified config item \n")
 	fmt.Printf("  -cg, --config-get <config item path>                        Get value to specified config item \n")
-	fmt.Printf("  -d,  --debug                                                Print more error detail\n")
 	fmt.Printf("  -f,  --follow                                               Follow mode - follow log output\n")
 	fmt.Printf("  -g,  --grok <grok pattern name>                             For non-json log line. The default patterns are saved in [%s]\n", defaultGrokLibraryDirs)
-	fmt.Printf("  -h,  --help                                                 Display this information\n")
 	fmt.Printf("  -j,  --json                                                 Output the raw JSON but then able to apply filters\n")
 	fmt.Printf("  -l,  --level <level value>                                  Filter by log level. For ex. --level warn \n")
 	fmt.Printf("  -n,  --lines <number of tail lines>                         Number of tail lines. 10 by default, for follow mode\n")
 	fmt.Printf("       --reset-grok-library-dir                               Save default GROK patterns to [%s]\n", defaultGrokLibraryDirs)
-	fmt.Printf("  -t,  --template                                             Print a config YAML file template\n")
-	fmt.Printf("  -V,  --version                                              Display app version information\n")
+	fmt.Printf("  -t,  --template                                             Print a convertion config YAML file template\n")
 	fmt.Println()
 }
